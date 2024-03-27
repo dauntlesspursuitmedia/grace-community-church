8cdc9e0b737b439dbb29a33df882f62541828764
Luke Miller <lukem1293@gmail.com>
2024-03-27 15:36:21 -0500

    added map embed

--- app/components/modules/ContactFormSection.tsx
+++ app/components/modules/ContactFormSection.tsx
@@ -7,13 +7,12 @@ export const ContactFormSection = () => {
       <div className="flex flex-col gap-8 @lg:flex-row justify-center">
         <ContactForm />
         <div>This is a test for the map embed</div>
-        {/* <iframe
-        src={`https://www.google.com/maps/embed/v1/place?q=4969%20McMinnville%20Hwy%20Manchester%2c%20TN&zoom=15&key=AIzaSyBnjnL6E1MNmfbfYNABMfBzYiS80Xv00D4&attribution_source=Google+Maps+Embed+API&attribution_web_url=https://developers.google.com/maps/documentation/embed/`}
-        title="Country Barn Construction Map"
-        width="100%"
-        height="500"
-        frameBorder="0"
-      /> */}
+        <iframe
+          src={`https://www.google.com/maps/embed/v1/place?q=4969%20McMinnville%20Hwy%20Manchester%2c%20TN&zoom=15&key=AIzaSyByOLkDp5PQpf-IDGXeTd-JkDRm8HlZmkQ&attribution_source=Google+Maps+Embed+API&attribution_web_url=https://developers.google.com/maps/documentation/embed/`}
+          title="Grace Community Church Map"
+          width="100%"
+          height="500"
+        />
       </div>
     </section>
   );
