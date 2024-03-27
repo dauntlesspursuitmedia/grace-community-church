3897d9f7d3929178947c1633f8f2561cb0f730ce
Luke Miller <lukem1293@gmail.com>
2024-03-27 15:39:34 -0500

    fixed contact section

--- app/components/modules/UiComponentRef.tsx
+++ app/components/modules/UiComponentRef.tsx
@@ -1,13 +1,13 @@
-import { lookup } from "~/lib/pageModules";
-import { ContactForm } from "../ContactForm";
+
 import { z } from "zod";
 import { uiComponentRefZ } from "~/types/shared";
 import { LatestSermonsList } from "../LatestSermons";
+import { ContactFormSection } from "./ContactFormSection";
 
 export const UiComponentRef = ({ name }: z.infer<typeof uiComponentRefZ>) => {
 
 
-	if(name === "contactForm") return <ContactForm />
+	if(name === "contactForm") return <ContactFormSection />
 
 	return <LatestSermonsList className="container px-8 max-w-max my-8 mx-auto" theme="dark" />
 
