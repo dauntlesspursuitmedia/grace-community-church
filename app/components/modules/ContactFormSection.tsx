30ce55d71269ffca5be3166eb7784fa8a07a3a6a
Luke Miller <lukem1293@gmail.com>
2024-03-27 15:43:05 -0500

    final fixes on contact form

--- app/components/modules/ContactFormSection.tsx
+++ app/components/modules/ContactFormSection.tsx
@@ -6,7 +6,6 @@ export const ContactFormSection = () => {
     <section className="container mx-auto @container">
       <div className="flex flex-col gap-8 @lg:flex-row justify-center">
         <ContactForm />
-        <div>This is a test for the map embed</div>
         <iframe
           src={`https://www.google.com/maps/embed/v1/place?q=4969%20McMinnville%20Hwy%20Manchester%2c%20TN&zoom=15&key=AIzaSyByOLkDp5PQpf-IDGXeTd-JkDRm8HlZmkQ&attribution_source=Google+Maps+Embed+API&attribution_web_url=https://developers.google.com/maps/documentation/embed/`}
           title="Grace Community Church Map"
